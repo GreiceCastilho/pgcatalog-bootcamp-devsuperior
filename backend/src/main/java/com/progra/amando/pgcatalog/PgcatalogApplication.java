abdb4ea5f0a03fe8dd3359aa9762cefa14645145
Greice Castilho <greicecfarma2@gmail.com>
2021-09-21 10:08:31 -0300

    First commit

---
+++ backend/src/main/java/com/progra/amando/pgcatalog/PgcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.progra.amando.pgcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/progra/amando/pgcatalog/PgcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra.amando.pgcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
