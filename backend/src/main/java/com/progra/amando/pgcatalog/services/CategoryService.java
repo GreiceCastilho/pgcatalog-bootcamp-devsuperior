4930695b96b268df87ad59db14a325f1d67a7dc4
Greice Castilho <greicecfarma2@gmail.com>
2021-10-07 10:19:44 -0300

    DTO

--- backend/src/main/java/com/progra/amando/pgcatalog/services/CategoryService.java
+++ backend/src/main/java/com/progra/amando/pgcatalog/services/CategoryService.java
@@ -1,23 +1,25 @@
 package com.progra.amando.pgcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.progra.amando.pgcatalog.dto.CategoryDTO;
 import com.progra.amando.pgcatalog.entities.Category;
 import com.progra.amando.pgcatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
-	@Autowired	
-	private CategoryRepository repository;
-	
-	@Transactional(readOnly = true) 	
-	public List<Category> findAll() {
-		return repository.findAll();		
-	}	
 
+	@Autowired
+	private CategoryRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
 }
